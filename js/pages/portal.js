3ad7f3d5909fcf45e9c77d40dc48e015972962a3
Ne0ne1 <isadadashev777@gmail.com>
2026-08-21 17:35:04 +0300

    Добавить новую пару набережной Сунжи

--- js/pages/portal.js
+++ js/pages/portal.js
@@ -40,8 +40,8 @@ async function boot() {
 
   try {
     const [places, events] = await Promise.all([
-      fetch('data/places.json?v=7').then((r) => r.json()),
-      fetch('data/events.json?v=7').then((r) => r.json()),
+      fetch('data/places.json?v=8').then((r) => r.json()),
+      fetch('data/events.json?v=8').then((r) => r.json()),
     ]);
 
     const portalPlaces = buildPortalPlaces(places, events);
